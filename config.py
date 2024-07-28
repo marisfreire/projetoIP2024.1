f27132c58e466591d4cd4c37488725cef6587ef2
Mariana Freire <msf6@cin.ufpe.br>
2024-07-28 19:32:15 -0300

    Mudança do arquivo main.py para refletir a Programação Orientada a Objetos, adição da primeira versão do mapa para fins ilustrativos, inicialização do jogo

---
+++ config.py
@@ -0,0 +1,5 @@
+largura = 1280
+altura = 820 # Precisamos de espaço para o Timer e para o Quebra cabeça, então será um pouco maior do que
+# a formatação HD normal
+
+FPS = 60  # 60 frames por segundo, o suficiente para o jogo rodar bem
--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-import pygame
-
-pygame.init()
-screen = pygame.display.set_mode((1280, 820))
-clock = pygame.time.Clock()
-running = True
-dt = 0
+import pygame as py
+from config import *
 
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # Ação de clicar no X da janela
-            running = False
-
-    screen.fill('white')
-
-    pygame.display.flip()
-
-    clock.tick(120)
-    dt = clock.tick(120) / 1000
+class Jogo:
+    def __init__(self):
+        # Setup geral do jogo
+        py.init()
+        self.tela = py.display.set_mode((largura, altura))  # tamanho da tela
+        self.nome_oficial = 'CinEncontre'
+        self.nome = py.display.set_caption(self.nome_oficial)
+        self.clock = py.time.Clock()
 
 
-pygame.quit()
+    def mostrar_tela(self):
+        while True:
+
+            for event in py.event.get():
+                if event.type == py.QUIT:
+                    py.quit()
+
+            mapa = py.image.load('mapa_cincontre.png')
+            rect_fundo = mapa.get_rect(topleft=(0, 100))
+            self.tela.fill('white')
+            self.tela.blit(mapa, rect_fundo)
+            py.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':  # jogo só será iniciado a partir do arquivo main
+    jogo = Jogo()
+    jogo.mostrar_tela()
