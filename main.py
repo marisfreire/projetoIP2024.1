6b3265ba3091c112468ad678767071853722cc06
Mariana Sousa Freire <msf6@WINDOWS.CIN.UFPE.BR>
2024-08-02 11:30:35 -0300

    Integração dos códigos de Giovanna e Rafael

--- main.py
+++ main.py
@@ -36,6 +36,9 @@ class Wall(object):
         self.image = py.image.load('wall.png')
 
 
+dano = []
+morreu = False
+
 class Jogo:
     def __init__(self):
         # Setup geral do jogo
@@ -55,6 +58,9 @@ class Jogo:
         self.tela.blit(msg, msg_rect)
 
     def menu(self, lista_walls, lista_player, lista_monsters):
+        # Reiniciando variáveis
+        global morreu
+        morreu = False # Reiniciando
         # Loop do jogo
         while True:
             key = py.key.get_pressed()
@@ -72,7 +78,12 @@ class Jogo:
                     quit()
 
     def play(self, lista_walls, lista_player, lista_monsters):
+        invincible_timer = 0  # Inicializa o temporizador de invencibilidade
         while True:
+            # Atualiza o temporizador de invencibilidade
+            if invincible_timer > 0:
+                invincible_timer -= 1
+
             # Desenho dos elementos: paredes, jogador e inimigos
             background = py.image.load('mapa_cincontre.png')
             self.tela.fill('white')
@@ -90,8 +101,13 @@ class Jogo:
             for monster in monsters:
                 py.draw.rect(self.tela, 'blue', monster.rect)
                 monster.move()
-                if check_collision(player, monster):  # Detecta as colisões entre os inimigos e os players
-                    self.derrota()
+                if check_collision(player, monster) and invincible_timer == 0:  # Detecta as colisões entre os inimigos e os players
+                    dano.append('dano')
+                    invincible_timer = 30 # 60 frames = 1 segundo de invicibilidade
+                    if len(dano) == 3:
+                        self.morreu = True
+                        self.derrota(lista_walls, lista_player, lista_monsters)
+                        
 
             key = py.key.get_pressed()
             if key[py.K_LEFT] or key[py.K_a]:
@@ -108,27 +124,66 @@ class Jogo:
                     quit()
             pygame.display.update()
 
-    def derrota(self):
+    def derrota(self, lista_walls, lista_player, lista_monsters):
         # tela de derrota
         while True:
-            self.tela.fill(black)
-            self.mensagem_tela("game over", 100, 300, white, 200)
-
-            key = py.key.get_pressed()
             for event in py.event.get():
-                if event.type == py.QUIT or key[py.K_ESCAPE]:
+                if event.type == py.QUIT:
                     quit()
+                    
+                if event.type == py.KEYDOWN:
+                    if event.key == py.K_RETURN:  # Reinicia o jogo ao pressionar Enter
+                        self.menu(lista_walls, lista_player, lista_monsters)
+                        # Sai da tela de derrota e retorna ao loop principal
+                        #colocar para voltar para a tela inicial 
+                    if event.key == py.K_ESCAPE:  # Sai do jogo ao pressionar Escape
+                        quit()
+                        
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if reinicio_button.collidepoint(event.pos):
+                            self.menu(lista_walls, lista_player, lista_monsters)
+                    if tela_inicial_button.collidepoint(event.pos):
+                            quit()
+
+            # Imagem de fundo
+            background = py.Surface(self.tela.get_size())
+            background.fill('black')
+            self.tela.blit(background, (0, 0))
+
+            # Desenhar o retângulo verde diretamente na tela
+            reinicio_button = pygame.draw.rect(self.tela, '#307225', (280, 480, 250, 100))
+            #fonte = pygame.font.Font(None, 30)
+            #reinicio_text = fonte.render(f'{'PLAY AGAIN'}',True,'red')
+            #self.tela.blit(reinicio_text, reinicio_button)
+
+
+            tela_inicial_button = pygame.draw.rect(self.tela, '#a93535', (750, 480, 250, 100)) #(x,y (do canto superior do retangulo), width, height)
+            #fonte = pygame.font.Font(None, 30)
+            #tela_inicial_text = fonte.render(f'{'TELA INICIAL'}',True,'green')
+            #self.tela.blit(tela_inicial_text, tela_inicial_button)
+
+        
+            # Título
+            self.mensagem_tela('GAME OVER', 375, 120, '#ffffff', 100)
+                    
+            self.mensagem_tela(
+                'Poxaaa...Infelizmente você perdeu seu crachá',
+                640, 270, 'White', 40
+            )
+
+            self.mensagem_tela(
+                'Pressione ENTER para reiniciar ou ESC para sair',
+                640, 400, 'White', 30
+            )
             pygame.display.update()
 
 
-def mapa_jogo(mapa):
+def mapa_jogo(mapa, x=0, y=100):
     walls = []
     player = []
     monsters = []
 
     # Análise da sequência de nível acima. 0 = parede, 3 = inimigo, 4 = player
-    x = 0
-    y = 100
     for row in mapa:
         for col in row:
             col = int(col)
